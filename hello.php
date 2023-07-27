c3f26fbdb498f94472e61e6c072e30a52bc4fd92
Eman Yassin <eman.yassin83@gmail.com>
2023-07-27 17:55:20 +0200

    adding comments

--- hello.php
+++ hello.php
@@ -1,3 +1,13 @@
 <?php
+
+// opening tag is <?php
+// this tag informs the server that there is some php code about to follow
+
+
+// echo construct it says whatever follow is to be printed to the browser
+
+// next to the echo construct is a string of text, so the echo knows when the text starts and ends 
+// we close the statement with a semicolon
+
 echo "Hello, world!";
 
