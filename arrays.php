51b86b22d116f985cf943f7f28dc5fd61f941f83
Eman Yassin <eman.yassin83@gmail.com>
2023-08-02 13:48:30 +0200

    associative arrays in php

--- arrays.php
+++ arrays.php
@@ -26,3 +26,31 @@ var_dump($articles); // this will work
 // we can access the elements of an array using the index of the element same as javascript
 
 var_dump("first article is {$articles[0]}");
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// we can manually set the index of an array element like this:
+
+$topic = [
+    10 => "PHP",
+    20 => "HTML",
+    30 => "CSS"
+];
+
+//here we are setting the index of the array elements to 10, 20, 30 so php is index 10 not 0
+
+
+
+// Associative arrays:
+//--------------------
+
+// Associative arrays are arrays that use key value pairs instead of indexes 
+// we can create associative arrays like this:
+
+$topic = [
+    "name" => "PHP",
+    "type" => "backend"
+];
+
+//var_dump($topic);
+var_dump("The name of the topic is {$topic['name']}");
